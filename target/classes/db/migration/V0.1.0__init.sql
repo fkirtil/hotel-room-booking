088c36cc942a295e7b56a44c261cc236b80be5fb
Fatma <fatma@Fatma-MacBook-Air.local>
2024-06-21 17:53:58 +0300

    Removed the target folder

--- target/classes/db/migration/V0.1.0__init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS room
-(
-    id              BIGINT          AUTO_INCREMENT PRIMARY KEY,
-    room_number     VARCHAR(45)     DEFAULT NULL,
-    capacity        SMALLINT        DEFAULT NULL,
-    sea_view        BOOLEAN         NOT NULL DEFAULT FALSE,
-    jacuzzi         BOOLEAN         NOT NULL DEFAULT FALSE,
-    safe_box        BOOLEAN         NOT NULL DEFAULT FALSE
-);
